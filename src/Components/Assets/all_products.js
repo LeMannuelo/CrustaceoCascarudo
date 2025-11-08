aa7dd0b0efdc61d8a3e162c7b5da958b1951e185
lmartd <lmartinezdu@unal.edu.co>
2025-11-08 01:09:35 -0500

    +

--- src/Components/Assets/all_products.js
+++ src/Components/Assets/all_products.js
@@ -1,6 +1,13 @@
 import cangri1 from "./cangri1.png"
 import cangri2 from "./cangri2.png"
 import cangri3 from "./cangri3.png"
+import aroscebolla from "./aroscebolla.png"
+import yuca from "./yuca.png"
+import papas from "./papas.png"
+import malteada1 from "./malteada1.png"
+import malteada2 from "./malteada2.png"
+import malteada3 from "./malteada3.png"
+
 
 let all_products = [
     {
@@ -10,20 +17,71 @@ let all_products = [
         image: cangri1,
         precio: 15.0,
     },
+
     {
         id: 2,
         name: "cangri 2",
         category: "hamburguesa",
         image: cangri2,
         precio: 20.0,
-    },    {
+    }, 
+
+    {
         id: 3,
         name: "cangri 3",
         category: "hamburguesa",
-        image: cangri3
-    ,
+        image: cangri3,
         precio: 25.0,
     },
+
+    {
+        id: 4,
+        name: "aroscebolla",
+        category: "acompañamientos",
+        image: aroscebolla,
+        precio: 5.0,
+    },
+
+    {
+        id: 5,
+        name: "yuca",
+        category: "acompañamientos",
+        image: yuca,
+        precio: 5.0,
+    },
+
+    {
+        id: 6,
+        name: "papas",
+        category: "acompañamientos",
+        image: papas,
+        precio: 5.0,
+    },
+
+    {
+        id: 7,
+        name: "malteada1",
+        category: "malteadas",
+        image: malteada1,
+        precio: 5.0,
+    },
+
+    {
+        id: 8,
+        name: "malteada2",
+        category: "malteadas",
+        image: malteada2,
+        precio: 5.0,
+    },
+
+    {
+        id: 9,
+        name: "malteada3",
+        category: "malteadas",
+        image: malteada3,
+        precio: 5.0,
+    },
+
 
 ];
 
